da497fc7d248af759fdb538d241d01167cc24b00
jys555 <jumanovyashnarbek55@gmail.com>
2025-06-14 10:54:30 +0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    title: "Raqsga tushadigan kaktus",
+    price: "149 000 so'm",
+    image: "https://via.placeholder.com/300x300?text=Kaktus",
+    category: "o‘yinchoqlar",
+    likes: 0
+  },
+  {
+    id: 2,
+    title: "Mini blender",
+    price: "199 000 so'm",
+    image: "https://via.placeholder.com/300x300?text=Blender",
+    category: "texnika",
+    likes: 0
+  },
+  // yana mahsulotlar qo‘shish mumkin
+];
